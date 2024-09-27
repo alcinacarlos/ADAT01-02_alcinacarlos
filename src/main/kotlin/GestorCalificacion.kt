05dd5047ced2f9e82d05072095a172a1bd3f1afd
alcinacarlos <calcrom0607@g.educaand.es>
2024-09-27 22:26:24 +0200

    terminado

--- src/main/kotlin/GestorCalificacion.kt
+++ src/main/kotlin/GestorCalificacion.kt
@@ -3,13 +3,17 @@ import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.notExists
 
-class GestorCalificacion(val filePath: Path) {
+class GestorCalificacion(private val filePath: Path) {
     init {
-        checkOrCreateFile()
+        checkFile()
     }
-    private fun checkOrCreateFile(){
 
+    private fun checkFile() {
+        if (filePath.notExists()) throw FileNotFoundException("Archivo no encontrado $filePath")
     }
+
+    /*Una función que reciba el fichero de calificaciones y devuelva una lista de diccionarios, donde cada diccionario contiene
+     la información de los exámenes y la asistencia de un alumno. La lista tiene que estar ordenada por apellidos.*/
     fun csvToListMaps(path: Path): List<Map<String, String>> {
         if (path.notExists()) throw FileNotFoundException("No existe el fichero $path")
         val br = Files.newBufferedReader(path)
@@ -26,15 +30,50 @@ class GestorCalificacion(val filePath: Path) {
         for (row in rows) {
             val map = emptyMap<String, String>().toMutableMap()
             for (i in headers.indices) {
-                map[headers[i]] = row[i]
+                map[headers[i]] = row[i].replace(",", ".")
             }
             data.add(map)
         }
         return data.drop(1)
     }
-    fun addFinalTestScores(data: List<Map<String, String>>) :List<Map<String, String>>{
-        data.forEach {
-            val parcial1 = it["Parcial1"]
+
+    /*
+    Una función que reciba una lista de diccionarios como la que devuelve la función anterior y añada a cada diccionario un nuevo
+    par con la nota final del curso. El peso de cada parcial de teoría en la nota final es de un 30% mientras que el peso del
+    examen de prácticas es de un 40%.
+    * */
+    fun addFinalTestScores(data: List<Map<String, String>>): List<Map<String, String>> {
+        return data.map { student ->
+            val parcial1 = student["Parcial1"]?.toDoubleOrNull()?: 0.0
+            val parcial2 = student["Parcial2"]?.toDoubleOrNull()?: 0.0
+            val practicas = student["Practicas"]?.toDoubleOrNull()?: 0.0
+            val notaFinal = parcial1.times(0.3) + parcial2.times(0.3) + practicas.times(0.4)
+            student + ("Ordinario1" to notaFinal.toString())
         }
     }
+
+    /*
+    Una función que reciba una lista de diccionarios como la que devuelve la función anterior y devuelva dos listas, una con los
+    alumnos aprobados y otra con los alumnos suspensos. Para aprobar el curso, la asistencia tiene que ser mayor o igual que el
+    75%, la nota de los exámenes parciales y de prácticas mayor o igual que 4 y la nota final mayor o igual que 5.
+    */
+    fun passOrFail(data: List<Map<String, String>>):Pair<List<String>, List<String>>{
+        val failed = mutableListOf("Alumnos suspensos: ")
+        val passed = mutableListOf("Alumnos aprobados: ")
+        data.forEach { student ->
+            val asistencia = (student["Asistencia"]?.replace("%", "")?.toDoubleOrNull()?: 0.0) >=75
+            val parcial1 = (student["Parcial1"]?.toDoubleOrNull()?: 0.0) >=4
+            val parcial2 = (student["Parcial2"]?.toDoubleOrNull()?: 0.0) >=4
+            val practicas = (student["Practicas"]?.toDoubleOrNull()?: 0.0) >=4
+            val ordinario1 = (student["Ordinario1"]?.toDoubleOrNull()?: 0.0) >= 5
+            val nombre = student["Apellidos"] + ", "+ student["Nombre"]
+            if (asistencia && parcial1 && parcial2 && practicas && ordinario1){
+                passed.add(nombre)
+            }else{
+                failed.add(nombre)
+            }
+        }
+        return Pair(passed, failed)
+    }
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
-import java.io.FileNotFoundException
-import java.nio.file.Files
 import java.nio.file.Path
-import kotlin.io.path.notExists
-
-// Lista[0]["Nombre"] = "JOSE ANTONIO"
 
 fun main() {
     val ruta = Path.of("src", "main", "resources", "calificaciones.csv")
     val gestorCalificacion = GestorCalificacion(ruta)
+    val csvList = gestorCalificacion.csvToListMaps(ruta)
+    val finalScores = gestorCalificacion.addFinalTestScores(csvList)
+    val passedAndFailed = gestorCalificacion.passOrFail(finalScores)
+    passedAndFailed.first.forEach { println(it) }
+    println("\n\n")
+    passedAndFailed.second.forEach { println(it) }
 }
 
